41e0aceb9633941d07241739a5f9b94e22c8065c
Martyna Lukaszewicz <martyna@uidaho.edu>
2020-04-29 23:01:05 -0700

    Update metator.py

--- metator/metator.py
+++ metator/metator.py
@@ -89,9 +89,9 @@ def download_and_install_dependencies():
     with tarfile.open(louvain_path, "r:gz") as tar:
         tar.extractall()
 
-    hmm_basename = dependencies["hmm_databases"].split("/")[-1].   !!!!!!!!!!!!!!!!!!!!!!!!!
-    hmm_path = share_dir / hmm_basename !!!!!!!
-    (cache_dir / hmm_basename).replace(hmm_path) !!!
+    hmm_basename = dependencies["hmm_databases"].split("/")[-1]
+    hmm_path = share_dir / hmm_basename 
+    (cache_dir / hmm_basename).replace(hmm_path) 
 
     prodigal_basename = dependencies["prodigal"].split("/")[-1]
     prodigal_path = tools_dir / "prodigal"
